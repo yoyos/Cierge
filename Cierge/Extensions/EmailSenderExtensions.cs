1b2a3596f47a9de0dc86f9e9920123e636431681
Biarity <biarity@outlook.com>
2017-12-24 18:27:50 +1000

    better messages

--- Cierge/Extensions/EmailSenderExtensions.cs
+++ Cierge/Extensions/EmailSenderExtensions.cs
@@ -14,24 +14,24 @@ namespace Cierge.Services
             {
                 case AuthOperation.AddingOtherUserEmail:
                     subject = "Adding email failed.";
-                    message = $"The email '{email}' is already in use by another account! " +
-                        $"Try logging out then logging in with that email instead. " +
+                    message = $"The email '{email}' is already in use by another account! <br/>" +
+                        $"Try logging out then logging in with that email instead. <br/>" +
                         $"If you still need to add it to this account then delete the other one.";
                     break;
                 case AuthOperation.AddingNovelEmail:
                     subject = "Add this email to your account";
-                    message = $"To add this email to your account, please click this link: <a href='{link}'>link</a>" +
-                        $"Alternatively, use this code: {token}";
+                    message = $"To add this email to your account, please <a href='{link}'>click here</a>. <br/>" +
+                        $"Alternatively, use this code: {token}.";
                     break;
                 case AuthOperation.Registering:
                     subject = "Create your account";
-                    message = $"To continue creating your account, please click this link: <a href='{link}'>link</a>" +
-                        $"Alternatively, use this code: {token}";
+                    message = $"To continue creating your account, please <a href='{link}'>click here</a>. <br/>" +
+                        $"Alternatively, use this code: {token}.";
                     break;
                 case AuthOperation.LoggingIn:
                     subject = "Login to your account";
-                    message = $"To login to your account, please click this link: <a href='{link}'>link</a>" +
-                        $"Alternatively, use this code: {token}";
+                    message = $"To login to your account, please <a href='{link}'>click here</a>. <br/>" +
+                        $"Alternatively, use this code: {token}.";
                     break;
                 default:
                     break;
