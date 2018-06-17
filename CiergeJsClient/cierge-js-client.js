18f27b2e839e92adfac0cdb9378914fc22aedd3c
Biarity <biarity@outlook.com>
2018-06-17 15:07:14 +1000

    Index file for cierge-js-client

--- CiergeJsClient/cierge-js-client.js
+++ CiergeJsClient/cierge-js-client.js
@@ -1,2 +1,2 @@
-alert("FUCK YOU.");
+window.location.href = "https://localhost:9000/connect/authorize?response_type=token&client_id=client-app&redirect_uri=http://localhost:8000/signin-oidc&scope=openid&state=skdhrghyiousdrhbg&nonce=sdfgtjhdfgjdfg&prompt=none";
 //# sourceMappingURL=cierge-js-client.js.map
